32d867e6ab538186dccd14f483423476b5eed554
Vladimir Ivan <VladimirIvan@users.noreply.github.com>
2025-10-24 15:30:09 +0100

    Update Cython version to 3.1.5

--- pythonforandroid/recipes/cython/__init__.py
+++ pythonforandroid/recipes/cython/__init__.py
@@ -3,7 +3,7 @@ from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 
 class CythonRecipe(CompiledComponentsPythonRecipe):
 
-    version = '0.29.36'
+    version = '3.1.5'
     url = 'https://github.com/cython/cython/archive/{version}.tar.gz'
     site_packages_name = 'cython'
     depends = ['setuptools']
