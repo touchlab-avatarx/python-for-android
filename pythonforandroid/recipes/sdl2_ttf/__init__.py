3ff0f5214cb9fe42b8742bf23a76182b8f90123c
Vladimir Ivan <VladimirIvan@users.noreply.github.com>
2025-10-29 16:20:51 +0000

    Implement get_include_dirs and prebuild_arch methods
    Add methods to get include directories and handle prebuild for architectures.

--- pythonforandroid/recipes/sdl2_ttf/__init__.py
+++ pythonforandroid/recipes/sdl2_ttf/__init__.py
@@ -6,5 +6,35 @@ class LibSDL2TTF(BootstrapNDKRecipe):
     url = 'https://github.com/libsdl-org/SDL_ttf/releases/download/release-{version}/SDL2_ttf-{version}.tar.gz'
     dir_name = 'SDL2_ttf'
 
+    def get_include_dirs(self, arch):
+        return [
+            os.path.join(self.ctx.bootstrap.build_dir, "jni", "SDL2_ttf", "include")
+        ]
+
+    def prebuild_arch(self, arch):
+        # We do not have a folder for each arch on BootstrapNDKRecipe, so we
+        # need to skip the external deps download if we already have done it.
+
+        build_dir = self.get_build_dir(arch.arch)
+
+        with open(os.path.join(build_dir, ".gitmodules"), "r") as file:
+            for section in file.read().split('[submodule "')[1:]:
+                line_split = section.split(" = ")
+                # Parse .gitmodule section
+                clone_path, url, branch = (
+                    os.path.join(build_dir, line_split[1].split("\n")[0].strip()),
+                    line_split[2].split("\n")[0].strip(),
+                    line_split[-1].strip()
+                )
+                # Clone if needed
+                if not os.path.exists(clone_path) or not os.listdir(clone_path):
+                    shprint(
+                        sh.git, "clone", url,
+                        "--depth", "1", "-b",
+                        branch, clone_path, "--recursive"
+                    )
+
+        super().prebuild_arch(arch)
+
 
 recipe = LibSDL2TTF()
