a7ef272f4f0167713b266678c6577f446277d46a
Vladimir Ivan <VladimirIvan@users.noreply.github.com>
2025-10-29 16:13:43 +0000

    Implement submodule cloning in SDL2_mixer recipe
    Add functionality to read and clone submodules from .gitmodules

--- pythonforandroid/recipes/sdl2_mixer/__init__.py
+++ pythonforandroid/recipes/sdl2_mixer/__init__.py
@@ -13,5 +13,24 @@ class LibSDL2Mixer(BootstrapNDKRecipe):
             os.path.join(self.ctx.bootstrap.build_dir, "jni", "SDL2_mixer", "include")
         ]
 
+    with open(os.path.join(build_dir, ".gitmodules"), "r") as file:
+            for section in file.read().split('[submodule "')[1:]:
+                line_split = section.split(" = ")
+                # Parse .gitmodule section
+                clone_path, url, branch = (
+                    os.path.join(build_dir, line_split[1].split("\n")[0].strip()),
+                    line_split[2].split("\n")[0].strip(),
+                    line_split[-1].strip()
+                )
+                # Clone if needed
+                if not os.path.exists(clone_path) or not os.listdir(clone_path):
+                    shprint(
+                        sh.git, "clone", url,
+                        "--depth", "1", "-b",
+                        branch, clone_path, "--recursive"
+                    )
+
+        super().prebuild_arch(arch)
+
 
 recipe = LibSDL2Mixer()
